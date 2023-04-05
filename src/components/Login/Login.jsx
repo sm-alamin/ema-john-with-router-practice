a9c34dc0bb45460ff146e49124605dc06799ccba
sm-alamin <sma.fkml@gmail.com>
2023-04-05 20:40:34 +0600

    router link is added on navigation bar

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h3>Hello from Login page</h3>
+        </div>
+    );
+};
+
+export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Shop from './components/Shop/Shop';
+import Home from './components/Layout/Home';
+import Inventory from './components/Inventory/Inventory';
+import Orders from './components/Orders/Orders';
+import Login from './components/Login/Login';
+
+const router = createBrowserRouter ([
+  {
+    path: "/",
+    element: <Home />,
+
+    children: [
+      {
+        path: "/",
+        element: <Shop />
+      },
+      {
+        path: "inventory",
+        element: <Inventory />
+      },
+      {
+        path: "orders",
+        element: <Orders />
+      },
+      {
+        path: "login",
+        element: <Login />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
